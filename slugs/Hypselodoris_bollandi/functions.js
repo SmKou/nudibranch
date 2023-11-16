85c2987a5d660ef7c0f1c2654a3c50385614be73
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-11-16 00:44:19 -0800

    Add header

--- slugs/Hypselodoris_bollandi/functions.js
+++ slugs/Hypselodoris_bollandi/functions.js
@@ -1,2 +1,16 @@
-import '/base.css'
 import './styles.css'
+
+const toggleVersion = ({ toggle, main }) => {
+    let version = false
+    toggle.addEventListener('click', () => {
+        main.classList.remove('version' + (version + 1))
+        version = !version
+        main.classList.add('version' + (version + 1))
+        toggle.innerHTML = `${version + 1}`
+    })
+}
+
+toggleVersion({
+    toggle: document.getElementById('toggle-version'),
+    main: document.querySelector('main')
+})
